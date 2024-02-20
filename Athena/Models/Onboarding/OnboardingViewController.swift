f9959cf010df7fade46e7a8cc6efa96115ec1ff2
Alexh Henbest <alex.henbest@gmail.com>
2024-02-20 11:28:57 -0500

    commit 3

--- Athena/Models/Onboarding/OnboardingViewController.swift
+++ Athena/Models/Onboarding/OnboardingViewController.swift
@@ -8,22 +8,22 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+    }
+   
+    
+    @IBAction func nextBtnClicked(_ sender: Any) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
