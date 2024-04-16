37e7d041d27aba624ca5f65ff1e7d5b62227dd5c
Alexh Henbest <alex.henbest@gmail.com>
2024-04-16 10:40:04 -0400

    Commit 21

---
+++ Athena/Models/DataFrame.swift
@@ -0,0 +1,8 @@
+//
+//  DataFrame.swift
+//  Athena
+//
+//  Created by Alex Henbest on 4/12/24.
+//
+
+import Foundation
---
+++ Athena/Models/triviaScreen.swift
@@ -0,0 +1,8 @@
+//
+//  triviaScreen.swift
+//  Athena
+//
+//  Created by Alex Henbest on 4/15/24.
+//
+
+import Foundation
