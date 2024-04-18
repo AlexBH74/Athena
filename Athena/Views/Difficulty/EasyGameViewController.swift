1e2decd2de138090deb49a459de2304df2aeb758
Alexh Henbest <alex.henbest@gmail.com>
2024-04-18 12:17:46 -0400

    Commit 2 to Game Controllers Branch

--- Athena/Views/Difficulty/EasyGameViewController.swift
+++ Athena/Views/Difficulty/EasyGameViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class EasyGameViewController: UIViewController {
     
+    private var correctAnswer: String?
     private var dataFrame: DataFrame?
     private var trivia: [triviaScreen] = []
     private var usedIndexes: Set<Int> {
@@ -73,12 +74,11 @@ class EasyGameViewController: UIViewController {
             
             let category = dataFrame!.rows[i][0]
             let question = dataFrame!.rows[i][2]
-            let correctAnswer = dataFrame!.rows[i][3]
+            correctAnswer = dataFrame!.rows[i][3]
             let answer1 = dataFrame!.rows[i][j[0]]
             let answer2 = dataFrame!.rows[i][j[1]]
             let answer3 = dataFrame!.rows[i][j[2]]
             let answer4 = dataFrame!.rows[i][j[3]]
-            print(correctAnswer)
             
             trivia = [triviaScreen(category: category, question: question, answer1: answer1, answer2: answer2, answer3: answer3, answer4: answer4)]
 
@@ -88,6 +88,47 @@ class EasyGameViewController: UIViewController {
             titleAnswer2.setTitle(trivia.first?.answer2, for: .normal)
             titleAnswer3.setTitle(trivia.first?.answer3, for: .normal)
             titleAnswer4.setTitle(trivia.first?.answer4, for: .normal)
+        }
+    }
+    
+    @IBAction func aClicked(_ sender: Any) {
+        let answer = correctAnswer
+        print(answer)
+        if answer == titleAnswer1.titleLabel?.text {
+            print("Correct!")
+        }
+        else {
+            print("Incorrect!")
+        }
+    }
+    @IBAction func bClicked(_ sender: Any) {
+        let answer = correctAnswer
+        print(answer)
+        if answer == titleAnswer2.titleLabel?.text {
+            print("Correct!")
+        }
+        else {
+            print("Incorrect!")
+        }
+    }
+    @IBAction func cClicked(_ sender: Any) {
+        let answer = correctAnswer
+        print(answer)
+        if answer == titleAnswer3.titleLabel?.text {
+            print("Correct!")
+        }
+        else {
+            print("Incorrect!")
+        }
+    }
+    @IBAction func dClicked(_ sender: Any) {
+        let answer = correctAnswer
+        print(answer)
+        if answer == titleAnswer4.titleLabel?.text {
+            print("Correct!")
+        }
+        else {
+            print("Incorrect!")
         }
     }
 }
